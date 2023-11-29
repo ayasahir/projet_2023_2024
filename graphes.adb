bb0e6b578c49b9acb8de89c800881e19af116041
ayasahir <139578130+ayasahir@users.noreply.github.com>
2023-11-29 19:55:34 +0100

    Add files via upload

---
+++ graphes.adb
@@ -0,0 +1,11 @@
+with Ada.Text_IO;            use Ada.Text_IO;
+with SDA_Exceptions;         use SDA_Exceptions;
+with Ada.Unchecked_Deallocation;
+
+package body graphes is
+
+
+
+
+
+end graphes;
---
+++ graphes.ads
@@ -0,0 +1,19 @@
+--Définition des graphes sous forme de tableau de tableaux
+
+package graphes is
+    --déclarations visibles
+
+private --??
+
+
+
+
+
+
+
+
+
+
+
+
+end graphes;
